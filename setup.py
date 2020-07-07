21072ea1c04b152d0222b31f31aec7e53d65aed6
b-remy <remy.benjamin.101@gmail.com>
2020-07-07 16:31:48 +0200

    editing setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='jax-lensing',
       version='0.1',
       description='',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/CosmoStat/jax-lensing',
       author='Benjamin Remy',
       author_email='benjamin.remy@cea.fr',
       license='MIT',
