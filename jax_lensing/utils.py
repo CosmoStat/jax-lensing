2ee7eb9f02a3b1c57c331cc1bd9355525a8cd781
Benjamin Remy <remy.benjamin.101@gmail.com>
2021-01-08 17:04:43 +0100

    crop to 512x512 images #13

--- jax_lensing/utils.py
+++ jax_lensing/utils.py
@@ -9,7 +9,7 @@ def load_dataset(batch_size, noise_dist_std, train_split):
     """ Pre-processing function preparing data for denoising task
     """
     # Cutout a portion of the map
-    x = tf.image.random_crop(tf.expand_dims(im['map'],-1), [320,320,1])
+    x = tf.image.random_crop(tf.expand_dims(im['map'],-1), [512,512,1])
     x = tf.image.random_flip_left_right(x)
     x = tf.image.random_flip_up_down(x)
     # Sample random Gaussian noise
@@ -19,7 +19,7 @@ def load_dataset(batch_size, noise_dist_std, train_split):
     # Create noisy image
     y = x + s * u
     return {'x':x, 'y':y, 'u':u,'s':s}
-  ds = tfds.load('massive_nu', split='train[:{}]'.format(train_split), shuffle_files=True)
+  ds = tfds.load('kappa_tng', split='train[:{}]'.format(train_split), shuffle_files=True)
   ds = ds.shuffle(buffer_size=10*batch_size)
   ds = ds.repeat()
   ds = ds.map(pre_process)
