76f21350116eb1928a6d07058f673108e3fa5b78
Benjamin Remy <remy.benjamin.101@gmail.com>
2021-01-14 15:00:20 +0100

    command line path specification

--- jax_lensing/datasets/kappatng.py
+++ jax_lensing/datasets/kappatng.py
@@ -17,6 +17,28 @@ It should also contain any processing which has been applied (if any),
 _CITATION = """
 """
 
+class KappaTNGConfig(tfds.core.BuilderConfig):
+  """BuilderConfig for KappaTNG."""
+
+  def __init__(self, *, resolution, **kwargs):
+    """BuilderConfig for SQUAD.
+    Args:
+      resolution: Resolution of the image. Values supported: powers of 2 up to
+        1024.
+      **kwargs: keyword arguments forwarded to super.
+    """
+    v2 = tfds.core.Version("2.0.0")
+    super(KappaTNGConfig, self).__init__(
+        name="%d" % resolution,
+        description=("KappaTNG images in %d x %d resolution" %
+                     (resolution, resolution)),
+        version=v2,
+        release_notes={
+            "2.0.0": "New split API (https://tensorflow.org/datasets/splits)",
+        },
+        **kwargs)
+    self.resolution = resolution
+    self.file_name = "kappaTNGCosmos.tar.gz"
 
 class Kappatng(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for kappatng dataset."""
@@ -25,6 +47,12 @@ class Kappatng(tfds.core.GeneratorBasedBuilder):
   RELEASE_NOTES = {
       '1.0.0': 'Initial release.',
   }
+
+  MANUAL_DOWNLOAD_INSTRUCTIONS = """\
+  manual_dir should contain a tar file with images (KappaTNGCosmos.tar.gz).
+  """
+  
+  BUILDER_CONFIGS = [KappaTNGConfig(resolution=1024)]
 
   def _info(self) -> tfds.core.DatasetInfo:
     """Returns the dataset metadata."""
@@ -47,11 +75,11 @@ class Kappatng(tfds.core.GeneratorBasedBuilder):
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
     # TODO(kappatng): Downloads the data and defines the splits
-    #path = dl_manager.download_and_extract('https://todo-data-url')
-    data_path = dl_manager.extract("/linkhome/rech/genpuc01/utb76xl/commun/kappaTNG/kappaTNGCosmos.tar.gz")
+    data_path = dl_manager.extract(os.path.join(dl_manager.manual_dir, self.builder_config.file_name)) 
 
     # TODO(kappatng): Returns the Dict[split names, Iterator[Key, Example]]
     return [
+
         tfds.core.SplitGenerator(
             name=tfds.Split.TRAIN,
             gen_kwargs={
