3186cb69223c9d95b271fcce52d4015a57f4d8ba
Benjamin Remy <remy.benjamin.101@gmail.com>
2021-03-03 17:08:28 +0100

    multiple samples

--- scripts/sample_maps.py
+++ scripts/sample_maps.py
@@ -2,7 +2,11 @@
 import os
 # This line is for running on Jean Zay
 #os.environ['XLA_FLAGS']='--xla_gpu_cuda_data_dir=/gpfslocalsys/cuda/10.0'
+#os.environ['XLA_FLAGS']='--xla_gpu_cuda_data_dir=/gpfslocalsys/cuda/10.2.1'
 os.environ['XLA_FLAGS']='--xla_gpu_cuda_data_dir=/gpfslocalsys/cuda/10.1.2'
+
+#os.environ['XLA_FLAGS']='--xla_gpu_cuda_data_dir=/gpfslocalsys/cuda/10.1'
+
 
 from absl import app
 from absl import flags
@@ -167,7 +171,7 @@ def main(_):
   print('final max temperature', onp.max(trace[1][:,-1]))
   # TODO: apply final projection
   # Save the chain
-  fits.writeto(FLAGS.output_file, onp.array(samples),overwrite=True)
+  fits.writeto("./results/samples_gp"+FLAGS.output_file+".fits", onp.array(samples),overwrite=True)
 
 if __name__ == "__main__":
   app.run(main)
